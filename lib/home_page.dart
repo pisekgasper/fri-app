58652453e1f83d86acc0961b065e3ec2b965f84c
pisekgasper <gasperpisek11@gmail.com>
2020-12-30 18:38:57 +0100

    :ok_hand: IMPROVED: Home page

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,10 +1,8 @@
-import 'package:flutter/services.dart';
-import 'package:fri_app/authentication_service.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:fri_app/buttons/huge_button.dart';
 import 'package:fri_app/nav_bar.dart';
-import 'package:provider/provider.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -12,111 +10,110 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  FirebaseFirestore db = FirebaseFirestore.instance;
+  FirebaseAuth auth = FirebaseAuth.instance;
+
+  String _fullName;
+  String _studentsNumber;
+
+  @override
+  void initState() {
+    super.initState();
+    getStudentsData().then((value) => setState(() {
+          _fullName = value['name'];
+          _studentsNumber = value['studentsNumber'].toString();
+        }));
+  }
+
   @override
   Widget build(BuildContext context) {
     final _screenWidth = MediaQuery.of(context).size.width;
     final _screenHeight = MediaQuery.of(context).size.height;
 
+    final double _fullNameFontSize = _screenHeight / 25;
+    final double _studentsNumberFontSize = _screenHeight / 40;
+
+    final TextStyle _fullNameTextStyle = TextStyle(
+      fontFamily: 'SF Pro Display',
+      fontSize: _fullNameFontSize,
+      color: Colors.white,
+      fontWeight: FontWeight.w700,
+    );
+
+    final TextStyle _studentsNumberTextStyle = TextStyle(
+      fontFamily: 'SF Pro Display',
+      fontSize: _studentsNumberFontSize,
+      color: Colors.white,
+      fontWeight: FontWeight.w100,
+    );
+
     return Scaffold(
       backgroundColor: const Color(0xff2c2f34),
-      body:
-      Column(
-        children: [
-          NavBar(title: "", back: false, user: true),
-          Container(
-            width: _screenWidth,
-            height: (_screenHeight - _screenWidth) / 2 - 50,
-            color: Colors.transparent,
-            child: Stack(
-              children: [
-                Align(
-                  alignment: Alignment.bottomCenter,
-                  child: Container(
-                    height: 90,
-                    child: Column(
-                      children: [
-                        Text(
-                          'Gašper',
-                          style: TextStyle(
-                            fontFamily: 'SF Pro Display',
-                            fontSize: 30,
-                            color: const Color(0xffffffff),
-                            letterSpacing: 1.7999999999999998,
-                            fontWeight: FontWeight.w700,
-                          ),
-                          textAlign: TextAlign.left,
-                        ),
-                        Text(
-                          '63190226',
-                          style: TextStyle(
-                            fontFamily: 'SF Pro Display',
-                            fontSize: 20,
-                            color: const Color(0xffffffff),
-                            fontWeight: FontWeight.w100,
-                          ),
-                          textAlign: TextAlign.left,
-                        ),
-                      ]
-                    ),
-                  ),
-                ),
-              ],
-            ),
+      body: Column(children: [
+        NavBar(title: "", back: false, user: true),
+        Container(
+          height: _fullNameFontSize * 4,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                height: _fullNameFontSize * 2,
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Text(
+                        (_fullName != null) ? _fullName : "",
+                        style: _fullNameTextStyle,
+                        textAlign: TextAlign.left,
+                      ),
+                      Text(
+                        (_studentsNumber != null) ? _studentsNumber : "",
+                        style: _studentsNumberTextStyle,
+                        textAlign: TextAlign.left,
+                      ),
+                    ]),
+              ),
+            ],
           ),
-          Center(
+        ),
+        Expanded(
           child: Container(
-            width: _screenWidth,
-            height: _screenWidth,
             child: Row(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Column(
-                  children: [
-                    Container(
-                      width: _screenWidth/2,
-                      height: _screenWidth/2,
-                      child: HugeButton(icon: Icons.calendar_today_rounded)
-                    ),
-                    Container(
-                      width: _screenWidth/2,
-                      height: _screenWidth/2,
-                      child: HugeButton(icon: Icons.directions_bus_rounded)
-                    ),
-                  ]
-                ),
-                Column(
-                  children: [
-                    Container(
-                      width: _screenWidth/2,
-                      height: _screenWidth/2,
-                      child: HugeButton(icon: Icons.fastfood_rounded)
-                    ),
-                    Container(
-                      width: _screenWidth/2,
-                      height: _screenWidth/2,
-                      child: HugeButton(icon: Icons.sticky_note_2_rounded)
-                    ),
-                  ]
-                ),
+                Column(children: [
+                  Container(
+                      width: _screenWidth / 2,
+                      height: _screenWidth / 2,
+                      child: HugeButton(icon: Icons.calendar_today_rounded)),
+                  Container(
+                      width: _screenWidth / 2,
+                      height: _screenWidth / 2,
+                      child: HugeButton(icon: Icons.directions_bus_rounded)),
+                ]),
+                Column(children: [
+                  Container(
+                      width: _screenWidth / 2,
+                      height: _screenWidth / 2,
+                      child: HugeButton(icon: Icons.fastfood_rounded)),
+                  Container(
+                      width: _screenWidth / 2,
+                      height: _screenWidth / 2,
+                      child: HugeButton(icon: Icons.sticky_note_2_rounded)),
+                ]),
               ],
             ),
           ),
-        ), ]
-      ),
-      /*body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("HOME"),
-            RaisedButton(
-              onPressed: () {
-                context.read<AuthenticationService>().signOut();
-              },
-              child: Text("Sign out"),
-            ),
-          ],
-        ),
-      ),*/
+        )
+      ]),
     );
+  }
+
+  Future<Map<String, dynamic>> getStudentsData() async {
+    Map<String, dynamic> result = new Map<String, dynamic>();
+    await db.collection('users').doc(auth.currentUser.uid).get().then(
+        (DocumentSnapshot documentSnapshot) =>
+            {if (documentSnapshot.exists) result = documentSnapshot.data()});
+    return result;
   }
 }
