4bb700fe07e42406cb5d19a2c55c52806c6a35a3
pisekgasper <gasperpisek11@gmail.com>
2020-12-30 18:31:15 +0100

    :ok_hand: IMPROVE: Navigation bar

--- lib/nav_bar.dart
+++ lib/nav_bar.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'buttons/round_button.dart';
 
 class NavBar extends StatelessWidget {
-
   const NavBar({
     this.title,
     this.back,
@@ -18,41 +17,51 @@ class NavBar extends StatelessWidget {
   Widget build(BuildContext context) {
     final _screenWidth = MediaQuery.of(context).size.width;
     final _screenHeight = MediaQuery.of(context).size.height;
+    final double _statusBarHeight = MediaQuery.of(context).padding.top;
+
+    final double _headingFontSize = _screenHeight / 30;
+    final double _buttonPadding = (_screenWidth / 10) / 2;
 
     return Container(
       width: _screenWidth,
       color: Colors.transparent,
-      height: 130,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.end,
+      margin: EdgeInsets.only(
+          top: _statusBarHeight + (_screenHeight / 70), bottom: _buttonPadding),
+      height: _screenWidth / 9,
+      child: Stack(
         children: [
-          Align(
-            alignment: Alignment.topLeft,
-            child: back ? RoundButton(icon: Icons.chevron_left_rounded) : null
+          Visibility(
+            visible: back,
+            child: Align(
+                alignment: Alignment.centerLeft,
+                child: Padding(
+                    padding: EdgeInsets.only(left: _buttonPadding),
+                    child: RoundButton(icon: Icons.chevron_left_rounded))),
           ),
-          Container(
-            color: Colors.transparent,
-            height: 85,
+          Align(
             alignment: Alignment.center,
             child: Text(
               title,
               style: TextStyle(
                 fontFamily: 'SF Pro Display',
-                fontSize: 32,
+                fontSize: _headingFontSize,
                 color: const Color(0xffffffff),
-                fontWeight: FontWeight.w600,
+                fontWeight: FontWeight.w700,
               ),
               textAlign: TextAlign.center,
             ),
           ),
-          Align(
-              alignment: Alignment.topRight,
-              child: user ? RoundButton(icon: Icons.person_rounded) : null
-          ),
+          Visibility(
+            visible: user,
+            child: Align(
+              alignment: Alignment.centerRight,
+              child: Padding(
+                  padding: EdgeInsets.only(right: _buttonPadding),
+                  child: RoundButton(icon: Icons.person_rounded)),
+            ),
+          )
         ],
       ),
     );
   }
-
 }
