fcfbd2688cd3c28121f80a755712137fd8804013
pisekgasper <gasperpisek11@gmail.com>
2021-01-03 18:45:57 +0100

    :ok_hand: IMPROVE: Refresh bus page

--- lib/bus_info.dart
+++ lib/bus_info.dart
@@ -3,7 +3,6 @@ import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'bus_info_model.dart';
 import 'get_bus.dart';
-import 'nav_bar.dart';
 
 class BusPage extends StatefulWidget {
   @override
@@ -11,10 +10,25 @@ class BusPage extends StatefulWidget {
 }
 
 class _BusPageState extends State<BusPage> with TickerProviderStateMixin {
+  void refresh() {
+    setState(() => {});
+  }
+
+  bool _isPressedLeft = false;
+  bool _isPressedRight = false;
+
   @override
   Widget build(BuildContext context) {
     final _screenWidth = MediaQuery.of(context).size.width;
     final _screenHeight = MediaQuery.of(context).size.height;
+    final double _statusBarHeight = MediaQuery.of(context).padding.top;
+
+    final double _buttonSize = _screenWidth / 9;
+    final double _iconSize = _screenWidth / 16;
+    final double _iconSizeLeft = _screenWidth / 14;
+
+    final double _headingFontSize = _screenHeight / 30;
+    final double _buttonPadding = (_screenWidth / 10) / 2;
 
     return Scaffold(
       backgroundColor: const Color(0xff2c2f34),
@@ -22,18 +36,129 @@ class _BusPageState extends State<BusPage> with TickerProviderStateMixin {
           // ...
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            NavBar(
-              title: "Avtobus",
-              back: true,
-              user: false,
-              refresh: true,
+            Container(
+              width: _screenWidth,
+              color: Colors.transparent,
+              margin: EdgeInsets.only(
+                  top: _statusBarHeight + (_screenHeight / 70),
+                  bottom: _buttonPadding),
+              height: _screenWidth / 9,
+              child: Stack(
+                children: [
+                  Align(
+                      alignment: Alignment.centerLeft,
+                      child: Padding(
+                          padding: EdgeInsets.only(left: _buttonPadding),
+                          child: Listener(
+                            onPointerDown: (PointerDownEvent event) {
+                              setState(() {
+                                _isPressedLeft = true;
+                              });
+                            },
+                            onPointerUp: (PointerUpEvent event) {
+                              setState(() {
+                                _isPressedLeft = false;
+                              });
+                              Navigator.pop(context);
+                            },
+                            child: Neumorphic(
+                              duration: const Duration(milliseconds: 80),
+                              style: NeumorphicStyle(
+                                  depth: !_isPressedLeft ? 5.0 : 0.0,
+                                  boxShape: NeumorphicBoxShape.circle()),
+                              child: AnimatedContainer(
+                                duration: const Duration(milliseconds: 80),
+                                decoration: BoxDecoration(
+                                  gradient: LinearGradient(
+                                    begin: Alignment(0.94, 0.92),
+                                    end: Alignment(-0.88, -0.89),
+                                    colors: _isPressedLeft
+                                        ? [
+                                            const Color(0xff2c2f34),
+                                            const Color(0xff2c2f34)
+                                          ]
+                                        : [
+                                            const Color(0xff282a2f),
+                                            const Color(0xff2f3238)
+                                          ],
+                                    stops: [0.0, 1.0],
+                                  ),
+                                ),
+                                width: _buttonSize,
+                                height: _buttonSize,
+                                child: Icon(Icons.chevron_left_rounded,
+                                    color: Colors.white, size: _iconSizeLeft),
+                              ),
+                            ),
+                          ))),
+                  Align(
+                    alignment: Alignment.center,
+                    child: Text(
+                      "Bus",
+                      style: TextStyle(
+                        fontFamily: 'SF Pro Display',
+                        fontSize: _headingFontSize,
+                        color: const Color(0xffffffff),
+                        fontWeight: FontWeight.w700,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: Padding(
+                        padding: EdgeInsets.only(right: _buttonPadding),
+                        child: Listener(
+                          onPointerDown: (PointerDownEvent event) {
+                            setState(() {
+                              _isPressedRight = true;
+                            });
+                          },
+                          onPointerUp: (PointerUpEvent event) {
+                            setState(() {
+                              _isPressedRight = false;
+                            });
+                          },
+                          child: Neumorphic(
+                            duration: const Duration(milliseconds: 80),
+                            style: NeumorphicStyle(
+                                depth: !_isPressedRight ? 5.0 : 0.0,
+                                boxShape: NeumorphicBoxShape.circle()),
+                            child: AnimatedContainer(
+                              duration: const Duration(milliseconds: 80),
+                              decoration: BoxDecoration(
+                                gradient: LinearGradient(
+                                  begin: Alignment(0.94, 0.92),
+                                  end: Alignment(-0.88, -0.89),
+                                  colors: _isPressedRight
+                                      ? [
+                                          const Color(0xff2c2f34),
+                                          const Color(0xff2c2f34)
+                                        ]
+                                      : [
+                                          const Color(0xff282a2f),
+                                          const Color(0xff2f3238)
+                                        ],
+                                  stops: [0.0, 1.0],
+                                ),
+                              ),
+                              width: _buttonSize,
+                              height: _buttonSize,
+                              child: Icon(Icons.refresh_rounded,
+                                  color: Colors.white, size: _iconSize),
+                            ),
+                          ),
+                        )),
+                  )
+                ],
+              ),
             ),
             Neumorphic(
               margin: EdgeInsets.symmetric(vertical: 40),
               style: NeumorphicStyle(
                 boxShape:
                     NeumorphicBoxShape.roundRect(BorderRadius.circular(30.0)),
-                depth: 7.0,
+                depth: 4.0,
               ),
               child: Container(
                 height: _screenHeight / 4,
@@ -122,7 +247,7 @@ class _BusPageState extends State<BusPage> with TickerProviderStateMixin {
                             ),
                           );
                         } else if (snapshot.hasError) {
-                          return Text("${snapshot.error}");
+                          return Text("No bus to center.");
                         }
                         return SpinKitWave(
                           color: Colors.white,
@@ -142,7 +267,7 @@ class _BusPageState extends State<BusPage> with TickerProviderStateMixin {
               style: NeumorphicStyle(
                 boxShape:
                     NeumorphicBoxShape.roundRect(BorderRadius.circular(30.0)),
-                depth: 7.0,
+                depth: 4.0,
               ),
               child: Container(
                 height: _screenHeight / 4,
@@ -231,7 +356,7 @@ class _BusPageState extends State<BusPage> with TickerProviderStateMixin {
                             ),
                           );
                         } else if (snapshot.hasError) {
-                          return Text("${snapshot.error}");
+                          return Text("No bus from center.");
                         }
                         return SpinKitWave(
                           color: Colors.white,
