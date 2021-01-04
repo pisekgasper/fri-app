e8345ddad337cd4e77ddca872708ae9078a252df
giknik <pangersicnik@gmail.com>
2021-01-04 14:08:10 +0100

    :construction: IN PROGRESS: Grades page

--- lib/grades.dart
+++ lib/grades.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 import 'package:fri_app/grades_info.dart';
 import 'package:fri_app/nav_bar.dart';
 
@@ -14,6 +15,7 @@ class _GradesPageState extends State<GradesPage> {
   final FirebaseFirestore db = FirebaseFirestore.instance;
 
   Map<String, String> _subjects;
+  List<bool> barr;
 
   @override
   void initState() {
@@ -24,6 +26,7 @@ class _GradesPageState extends State<GradesPage> {
         setState(
           () {
             _subjects = val;
+            barr = new List<bool>.filled(_subjects.length, false);
           },
         );
       },
@@ -32,44 +35,95 @@ class _GradesPageState extends State<GradesPage> {
 
   @override
   Widget build(BuildContext context) {
+    final _screenWidth = MediaQuery.of(context).size.width;
+    final _screenHeight = MediaQuery.of(context).size.height;
+
     return Scaffold(
       backgroundColor: const Color(0xff2c2f34),
-      body: Column(
-        children: [
-          NavBar(
-            title: "Grades",
-            back: true,
-            user: false,
-            refresh: false,
-          ),
-          Column(
-            children: (_subjects != null) ? listSubjects() : [],
-          ),
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            NavBar(
+              title: "Grades",
+              back: true,
+              user: false,
+              refresh: false,
+            ),
+            Column(
+              children: (_subjects != null)
+                  ? listSubjects(_screenHeight, _screenWidth)
+                  : [],
+            ),
+          ],
+        ),
       ),
     );
   }
 
-  List<Widget> listSubjects() {
+  List<Widget> listSubjects(double _sH, double _sW) {
     List<Widget> list = new List();
-    _subjects.forEach((code, name) {
-      list.add(
-        new InkWell(
-          onTap: () async {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => GradesInfo(
-                  subjectCode: code.toString(),
-                  subjectName: name.toString(),
+    _subjects.forEach(
+      (code, name) {
+        list.add(
+          GestureDetector(
+            onTapDown: (_) {
+              setState(
+                () {},
+              );
+            },
+            onTapUp: (_) {
+              setState(
+                () {},
+              );
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => GradesInfo(
+                    subjectCode: code.toString(),
+                    subjectName: name.toString(),
+                  ),
+                ),
+              );
+            },
+            child: Neumorphic(
+              margin: EdgeInsets.symmetric(vertical: _sH / 50),
+              style: NeumorphicStyle(
+                boxShape: NeumorphicBoxShape.roundRect(
+                  BorderRadius.circular(30.0),
+                ),
+                depth: 2.0,
+              ),
+              child: Container(
+                width: _sW - _sW / 8,
+                height: _sH / 11,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(left: (_sW / 15.0)),
+                      child: Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(name.toString()),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(right: (_sW / 15.0)),
+                      child: Align(
+                        alignment: Alignment.centerRight,
+                        child: Icon(
+                          Icons.chevron_right_rounded,
+                          color: Colors.white,
+                        ),
+                      ),
+                    )
+                  ],
                 ),
               ),
-            );
-          },
-          child: Text(name.toString()),
-        ),
-      );
-    });
+            ),
+          ),
+        );
+      },
+    );
     return list;
   }
 
